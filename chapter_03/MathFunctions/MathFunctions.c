c4e27491de063b2fc69640c1569d17a3322e9049
dlm <376958755@qq.com>
2017-03-21 14:11:03 +0800

    编译库并使用

---
+++ chapter_03/MathFunctions/MathFunctions.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "MathFunctions.h"
+
+void p()
+{
+	printf("%s\n", "库文件中的函数");
+}
---
+++ chapter_03/build/CMakeFiles/cmakeStu.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Volumes/C/WorkSpace/cmakeStu/chapter_03/main.c" "/Volumes/C/WorkSpace/cmakeStu/chapter_03/build/CMakeFiles/cmakeStu.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../MathFunctions"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Volumes/C/WorkSpace/cmakeStu/chapter_03/build/MathFunctions/CMakeFiles/MathFunctions.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
